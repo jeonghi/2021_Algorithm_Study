d8e2995d7bf554bddcf0d52c0ac69765c2d21339
jeonghwanPark <jpark0902@kookmin.ac.kr>
2021-01-13 15:03:02 +0900

    1월 13일. 회의실 배정 문제 알고리즘 (강의 코드 추가)

--- section4/회의실배정.py
+++ section4/회의실배정.py
@@ -1,6 +1,6 @@
-def is_acceptable_capacity(meeting_capacity, lst):
+def is_acceptable_capacity(meeting_capacity, meeting):
     from itertools import combinations
-    cases = list(combinations(lst, r=meeting_capacity))
+    cases = list(combinations(meeting, r=meeting_capacity))
     for case in cases:
         count = 1
         for t in range(0,len(case)-1):
@@ -12,19 +12,31 @@ def is_acceptable_capacity(meeting_capacity, lst):
             return True
     return False
 
-def solution(n, lst):
-    lst.sort(key=lambda x: x[0])
+def solution(n, meeting):
+    meeting.sort(key=lambda x: (x[0],x[1]))
     lt = 1
     rt = n
     answer = 1
     while(lt<=rt):
         meeting_capacity = (lt+rt)//2
-        if is_acceptable_capacity(meeting_capacity, lst) is True:
+        if is_acceptable_capacity(meeting_capacity, meeting) is True:
             answer = meeting_capacity
             lt += 1
         else:
             rt -= 1
     return answer
+
+def solution2(n, meeting): # 강의에서 풀이해준 코드
+    lst.sort(key=lambda x: (x[1], x[0]))
+    end_time = 0
+    count = 0
+    for start, end in meeting:
+        if start >= end_time :
+            end_time = end
+            count += 1
+    return count
+
+
 
 
 if __name__ == "__main__":
@@ -35,4 +47,5 @@ if __name__ == "__main__":
     for i in range(n):
         lst.append(tuple(map(int, input().split())))
     answer = solution(n, lst)
+    answer = solution2(n, lst)
     print("Answer is {}".format(answer))
