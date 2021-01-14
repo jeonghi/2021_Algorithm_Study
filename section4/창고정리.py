ece02fda8607eeef59129e4d3c0ab634ce9644dc
jeonghwanPark <jpark0902@kookmin.ac.kr>
2021-01-14 16:48:17 +0900

    1월 14일. 창고정리 문제 알고리즘 (정답코드 추가)

--- section4/창고정리.py
+++ section4/창고정리.py
@@ -5,13 +5,24 @@ def solution(m, lst):
     for i in range(m):
         rt = len(lst)-1
         lt = 0
-        while((lst[rt-1]>=lst[rt])):
-            rt-=1
+
+        while(lst[rt-1]>=lst[rt]):
+            rt -= 1
         while(lst[lt]>=lst[lt+1]):
-            lt+=1
+            lt += 1
+
         lst[rt] -= 1
         lst[lt] += 1
-    return (lst[-1]-lst[0])
+
+    return (lst[-1] - lst[0])
+
+def solution2(m, lst):
+    lst.sort(key=lambda x:x)
+    for i in range(m):
+        lst[-1] -= 1
+        lst[0] += 1
+        lst.sort()
+    return lst[-1]-lst[0]
 
 
 if __name__ == "__main__":
@@ -19,5 +30,5 @@ if __name__ == "__main__":
     l = int(input())
     lst = list(map(int, input().split()))
     m = int(input())
-    answer = solution(m, lst)
+    answer = solution2(m, lst)
     print(answer)
