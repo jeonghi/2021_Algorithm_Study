0b38933aa09b94e2cf14d66b4a1b82f8f412f78d
jeonghwanPark <jpark0902@kookmin.ac.kr>
2021-01-15 11:31:03 +0900

    1월 15일. 침몰하는 타이타닉 문제 알고리즘 (강의 코드 추가)

--- section4/침몰하는타이타닉.py
+++ section4/침몰하는타이타닉.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 
 def solution(m, lst):
     lst.sort()
@@ -15,9 +16,27 @@ def solution(m, lst):
             break
     return count
 
+def solution2(m, lst): # 강의풀이코드
+    lst.sort()
+    lst = deque(lst)
+    count = 0
+    while(lst):
+        if len(lst)==1:
+            count += 1
+            break
+        if lst[0]+lst[-1]>m:
+            lst.pop()
+            count += 1
+        else:
+            lst.popleft()
+            #lst.pop(0) # 리스트의 pop연산을 하게 되면 모든 요소 한칸씩 한칸씩 당겨지게 된다. deque를 사용하면 링크드 리스트로 되어있어서 더 효율적이다.
+            lst.pop()
+            count += 1
+    return count
+
 
 if __name__ == "__main__":
     sys.stdin = open("input7.txt", "r")
     n, m = map(int,input().split())
     lst = list(map(int, input().split()))
-    print(solution(m, lst))
+    print(solution2(m, lst))
