5feb55bf14be4680304431c021940f956c188b34
jeonghwanPark <jpark0902@kookmin.ac.kr>
2021-01-18 10:01:02 +0900

    1월 17일. 가장 큰 수 문제 알고리즘 (강의 코드 추가)

--- section5/가장큰수.py
+++ section5/가장큰수.py
@@ -1,12 +1,42 @@
 def solution(n, m):
     from itertools import combinations
-    n = str(n)
-    answer = max(list(map(lambda x: int("".join(x)), list(combinations(n, r=len(n)-m)))))
+    answer = max(map(lambda x: int("".join(x)), combinations(n, r=len(n)-m)))
     return answer
+
+def solution2(n, m):
+    from collections import deque
+    dq = deque(map(int,n))
+    lst = list()
+    while(len(dq)!=0):
+        if len(lst) == 0 :
+            lst.append(dq.popleft())
+        elif lst[-1] < dq[0] and len(lst)+len(dq)>len(n)-m:
+            lst.pop()
+        else:
+            if len(lst) == len(n)-m:
+                dq.popleft()
+            else:
+                lst.append(dq.popleft())
+    return "".join(map(str, lst))
+
+def solution3(n, m):
+    stk = list()
+    for i in n:
+        while stk and m>0 and stk[-1]<i:
+            stk.pop()
+            m -= 1
+        stk.append(i)
+
+    if m!= 0:
+        stk = stk[:-m]
+    return "".join(map(str, stk))
+
+
 
 
 if __name__ == "__main__":
     import sys
     sys.stdin = open("./testcase/input1.txt", "r")
-    n, m = map(int, input().split())
-    print(solution(n, m))
+    n, m = input().split()
+    m = int(m)
+    print(solution3(n, m))
