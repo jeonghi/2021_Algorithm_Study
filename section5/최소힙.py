d1bdbc1b9f3fab7147d30181737d44e70ad8a40d
jeonghwanPark <jpark0902@kookmin.ac.kr>
2021-01-26 16:24:28 +0900

    1월 26일. 최소힙/ 최대힙 자료구조 공부 (python 내장 모듈 heapq import)

---
+++ section5/최소힙.py
@@ -0,0 +1,26 @@
+def solution(prompt):
+    import heapq
+    heap = []
+    for p in prompt:
+        if p > 0 :
+            heapq.heappush(heap, p)
+        elif p == 0 :
+            if heap :
+                print(heapq.heappop(heap))
+            else:
+                print(-1)
+        else:
+            return
+
+
+
+
+
+if __name__ == "__main__" :
+
+    import sys
+    sys.stdin = open("testCase/input10.txt")
+    prompt = []
+    for line in sys.stdin:
+        prompt.append(int(line.strip()))
+    solution(prompt)
